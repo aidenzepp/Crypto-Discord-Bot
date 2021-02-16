4d95fc7399926a260cb42a83d90f7657bed2ea9e
aidenzepp <aiden.zepp@gmail.com>
2021-02-16 15:24:39 -0500

    Created 'bot.py' file.

---
+++ src/bot.py
@@ -0,0 +1,23 @@
+import discord
+from discord.ext import commands
+from re import L
+import random
+import time
+import os
+# --
+
+
+client = commands.Bot(command_prefix = '.')
+
+
+# -- Events -- 
+
+
+# -- Commands --
+
+
+# -- Load Cogs --
+
+
+# -- Run --
+client.run('ODExMjg1NDY2NjQwMDIzNjUy.YCv-eA.IeA6mMJiCtSKLA2EjxR0nXaF1xQ')
