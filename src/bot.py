5e3576a5d8652f44c98be9ba783105813e2bb642
aidenzepp <aiden.zepp@gmail.com>
2021-02-16 15:35:35 -0500

    Added a loop to load cogs on startup.

--- src/bot.py
+++ src/bot.py
@@ -32,7 +32,12 @@ async def reload(ctx, extension):
     client.unload_extension(f'cogs.{extension}')
     client.load_extension(f'cogs.{extension}')
 
+
 # -- Load Cogs --
+for filename in os.listdir('./cogs'):
+
+    if filename.endswith('.py'):
+        client.load_extension(f'cogs.{filename[:-3]}')
 
 
 # -- Run --
