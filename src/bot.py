07b540f9041df0e8b520e6a8f0fe17599b0f844d
aidenzepp <aiden.zepp@gmail.com>
2021-02-16 15:32:29 -0500

    Added commands to interact with a cog.

--- src/bot.py
+++ src/bot.py
@@ -17,7 +17,20 @@ async def on_ready():
 
 
 # -- Commands --
+@client.command()
+async def load(ctx, extension):
+    client.load_extension(f'cogs.{extension}')
 
+
+@client.command()
+async def unload(ctx, extension):
+    client.unload_extension(f'cogs.{extension}')
+
+
+@client.command()
+async def reload(ctx, extension):
+    client.unload_extension(f'cogs.{extension}')
+    client.load_extension(f'cogs.{extension}')
 
 # -- Load Cogs --
 
