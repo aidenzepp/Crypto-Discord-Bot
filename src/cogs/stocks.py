97fde1ef9c21102610c98528a0fe30ffb50a1c55
aidenzepp <aiden.zepp@gmail.com>
2021-03-02 16:04:03 -0500

    Fetching and storing data.

--- src/cogs/stocks.py
+++ src/cogs/stocks.py
@@ -1,3 +1,4 @@
+from inspect import indentsize
 from re import L
 import discord
 from discord.ext import commands
@@ -20,12 +21,43 @@ class Stocks(commands.Cog):
         self.keys = keys
 
 
+        # CoinMarketCap API Documentation - Quickstart Guide's Format
+        url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
+
+        parameters = {
+            'start': '1',
+            'limit': '5000',
+            'convert': 'USD'
+        }
+
+        headers = {
+            'Accepts': 'application/json',
+            'X-CMC_PRO_API_KEY': self.keys['coinmarketcap']
+        }
+
+
+        session = Session()
+        session.headers.update(headers)
+
+
+        try:
+            response = session.get(url, params = parameters)
+            data = json.loads(response.text)
+
+            with open('src/CMC_data.json', 'w') as f:
+                json.dump(data, f, indent = 4)
+
+        except (ConnectionError, Timeout, TooManyRedirects) as ERROR:
+            print('Encountered Error: ' + ERROR)
+        # --
+
+
 
     # -- Events --
 
 
 
-    # -- Commands --    
+    # -- Commands --
 
 
 def setup(client):
