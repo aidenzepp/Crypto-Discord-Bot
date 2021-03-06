da0b754d72246163e5e19b0e6a278976b6b6e874
aidenzepp <aiden.zepp@gmail.com>
2021-03-06 15:20:16 -0500

    Adds user info on joining.

--- src/cogs/welcome.py
+++ src/cogs/welcome.py
@@ -2,6 +2,7 @@ from re import L
 import discord
 from discord.ext import commands
 import random
+import json
 import time
 import os
 #--
@@ -14,6 +15,26 @@ class Welcome(commands.Cog):
 
 
     # -- Events --
+    @commands.Cog.listener()
+    async def on_member_join(self, member):
+        filepath = 'src/hidden/ALL_USERS_INFO.json'
+
+        with open(filepath) as f:
+            info = json.load(f)
+
+        info['users'].append({
+            'name': member.display_name,
+            'discriminator': member.discriminator,
+            'id': member.id,
+            'mention': member.mention,
+            'nickname': member.nick,
+            'colour': str(member.colour),
+            'joined_at': str(member.joined_at)
+        })
+
+        with open(filepath, 'w') as outfile:
+            json.dump(info, outfile, indent = 4)
+        
 
 
 
