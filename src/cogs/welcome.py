897c0998eba97e49a3f9cfd760ca4c09e5efcd0d
aidenzepp <aiden.zepp@gmail.com>
2021-03-06 14:38:34 -0500

    Created 'welcome.py' cog.

---
+++ src/cogs/welcome.py
@@ -0,0 +1,25 @@
+from re import L
+import discord
+from discord.ext import commands
+import random
+import time
+import os
+#--
+
+class Welcome(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+
+
+
+    # -- Events --
+
+
+
+    # -- Commands --
+
+
+
+def setup(client):
+    client.add_cog(Welcome(client))
