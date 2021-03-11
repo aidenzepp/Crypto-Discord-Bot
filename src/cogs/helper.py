e07456a39324a098b952f46afed66f88b3d3cd27
aidenzepp <aiden.zepp@gmail.com>
2021-03-11 13:37:15 -0500

    Added desc. and data.

--- src/cogs/helper.py
+++ src/cogs/helper.py
@@ -6,10 +6,78 @@ import json
 import os
 #--
 
+'''
+I. Description:
+    - Stores repeatedly used functions by one or multiple other cogs.
+    - Stores information used by one or multiple other cogs.
+    - Used to keep most of the functions, information, or other code
+      in one place; this allows for keeping the other cogs more clean.
+
+
+II. Using the 'Helper' Class:
+    - In the body of another cog's '__init__()', insert the code below:
+        >>> self.helper = Helper(client)
+
+
+III. CoinMarketCap Data:
+    - The CMC will be stored in the Helper Class.
+        [** This keeps the error of a circular import from occurring. **]
+
+[ ~~~ ]
+
+Functions List:
+> n/a
+'''
+
 class Helper(commands.Cog):
 
+    # -- Load API Keys --
+    with open('src/hidden/secrets.json') as f: 
+        secrets = json.load(f)
+        keys = secrets['keys']
+        # Because the code above is inserted this way,
+        # the 'keys' object can be accessed using 'self.keys'.
+        
     def __init__(self, client):
         self.client = client
+        self.hidden = 'src/hidden' # Path for the 'hidden' folder.
+        self.cogs = 'src/cogs' # Path for the 'cogs' folder.
+        self.data = None
+
+        # CoinMarketCap API Documentation - Quickstart Guide's Format
+        url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
+
+        parameters = {
+            'start': '1',
+            'limit': '100',
+            'convert': 'USD'
+        }
+
+        headers = {
+            'Accepts': 'application/json',
+            'X-CMC_PRO_API_KEY': self.keys['coinmarketcap']
+        }
+
+        session = Session()
+        session.headers.update(headers)
+
+        try:
+            response = session.get(url, params = parameters)
+            data = json.loads(response.text)
+
+            with open(f'{self.hidden}//CMC_data.json', 'w') as f:
+                json.dump(data, f, indent = 4)
+
+        except (ConnectionError, Timeout, TooManyRedirects) as ERROR:
+            print('[CMC API] Encountered Error: ' + ERROR)
+        # --
+
+    def __repr__(self):
+        pass
+
+    def __str__(self):
+        pass
+
 
     
 # -- Cog Setup --
