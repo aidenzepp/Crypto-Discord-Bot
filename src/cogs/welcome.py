a9b9139448d56279bd0a354700f35a013ecfcd0b
aidenzepp <aiden.zepp@gmail.com>
2021-03-11 14:13:06 -0500

    Now uses 'helper' cog.

--- src/cogs/welcome.py
+++ src/cogs/welcome.py
@@ -1,58 +1,26 @@
-from asyncio.windows_events import NULL
-from re import L
 import discord
 from discord.ext import commands
-import random
 import json
-import time
-import os
+
+from cogs.helper import Helper
 #--
 
 class Welcome(commands.Cog):
 
     def __init__(self, client):
         self.client = client
-
-    
-
-    # -- Functions --
-    async def create_user_info(self, member, filepath = NULL):
-        info = {}
-
-        # Storing in 'user' allows for separate entities (preferences, notifications, etc.) to be different keys.
-        info['user'] = {
-            'name': member.display_name,
-            'discriminator': member.discriminator,
-            'id': member.id,
-            'mention': member.mention,
-            'nickname': member.nick,
-            'colour': str(member.colour),
-            'joined_at': str(member.joined_at)
-        }
-
-        if filepath != NULL:
-            with open(filepath, 'w') as outfile:
-                json.dump(info, outfile, indent = 4)
-           
-            return
-
-        return info
-
+        self.helper = Helper(client)
 
 
     # -- Events --
     @commands.Cog.listener()
     async def on_member_join(self, member):
-        member_id = member.id
-        filepath = f'src/hidden/ALL_USERS_INFO/{member_id}.json'
-
-        await self.create_user_info(member, filepath)
-
+        await self.helper.make_user(member)
 
 
     # -- Commands --
 
 
-
+# -- Cog Setup --
 def setup(client):
     client.add_cog(Welcome(client))
