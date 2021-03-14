f6dc52785727d140aac995c650b1f5f021d56aed
aidenzepp <aiden.zepp@gmail.com>
2021-03-14 15:31:48 -0400

    Added startup confirmations.

--- src/bot.py
+++ src/bot.py
@@ -7,17 +7,98 @@ import json
 import os
 # --
 
+prefix = '.'
+
+# -- Run Confirmations --
+print('[ --- STARTUP CONFIRMATIONS: --- ]')
+time.sleep(1.0)
+confirm = input('Run confirmations? (y/n) [If not, the bot will startup immediately.]  |  Answer: ')
+
+if (confirm.upper() == 'Y') or (confirm.upper() == 'YES'):
+    time.sleep(1.0)
+    run_confs = True
+    print('---')
+    time.sleep(1.0)
+else:
+    time.sleep(1.0)
+    run_confs = False
+    print('Commencing immediate startup...')
+    time.sleep(0.5)
+    print(f'Bot Prefix: \'{prefix}\'')
+    time.sleep(1.0)
+    print('[ --- STARTUP CONFIRMATIONS: END --- ]')
+    time.sleep(1.0)
+
+if run_confs:
+    # -- Startup Confirmation --
+    startup_confirm = input('[Confirmation] Would you like to start the bot? (y/n)   |  Answer: ')
+
+    if (startup_confirm.upper() == 'Y') or (startup_confirm.upper() == 'YES'):
+        time.sleep(1.0)
+        print('Request for startup confirmed.')
+        time.sleep(1.0)
+        print('---')
+        time.sleep(1.0)
+    else:
+        time.sleep(1.0)
+        print('Request for startup denied. Exiting bot\'s startup process...')
+        time.sleep(1.0)
+        print('[ --- STARTUP CONFIRMATIONS: END --- ]')
+        time.sleep(1.0)
+        print('''
+
+        =====================
+         BOT STATUS: OFFLINE
+        =====================
+        
+        '''
+        )
+        time.sleep(1.0)
+        print('[ --- STARTUP CANCELED --- ]')
+        exit()
+
+    # -- Prefix Confirmation --
+    prefix_confirm = input(f'[Confirmation] Would you like to keep the command prefix set to \'{prefix}\'? (y/n)   |  Answer: ')
+
+    if (prefix_confirm.upper() == 'Y') or (prefix_confirm.upper() == 'YES') and run_confs:
+        time.sleep(1.0)
+        print(f'Request for maintaining prefix confirmed.')
+        time.sleep(0.5)
+        print(f'Bot Prefix: \'{prefix}\'')
+    else:
+        time.sleep(1.0)
+        prefix = input('What would you like the command prefix to be set to? ')
+        time.sleep(0.5)
+        print(f'Bot Prefix: \'{prefix}\'')
+
+    time.sleep(1.0)
+    print('[ --- STARTUP CONFIRMATIONS: END --- ]')
+    time.sleep(1.0)
+    
+
+
+# --
+
 
 intents = discord.Intents.default()
 intents.members = True
 
-client = commands.Bot(command_prefix = '.', intents = intents)
+client = commands.Bot(command_prefix = prefix, intents = intents)
 
 
 # -- Events --
 @client.event
 async def on_ready():
-    print('Bot is ready.')
+    print('''
+
+    =====================
+      BOT STATUS: READY
+    =====================
+    
+    '''
+    )
+    time.sleep(1.0)
+    print('[ --- STARTUP COMPLETE --- ]')
 
 
 
