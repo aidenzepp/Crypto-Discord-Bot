4068a0f8b7befb79e9d60b349826dd35f10ffa2f
aidenzepp <aiden.zepp@gmail.com>
2021-03-14 16:01:25 -0400

    Added more confirmations.

--- src/bot.py
+++ src/bot.py
@@ -7,20 +7,24 @@ import json
 import os
 # --
 
-prefix = '.'
+# -- Load Discord API Key --
+with open('src/hidden/secrets.json') as f: 
+    secrets = json.load(f)
+    keys = secrets['keys']
+    serverinfo = secrets['server']
+    prefix = serverinfo['prefix']
 
 # -- Run Confirmations --
 print('[ --- STARTUP CONFIRMATIONS: --- ]')
 time.sleep(1.0)
 confirm = input('Run confirmations? (y/n) [If not, the bot will startup immediately.]  |  Answer: ')
+time.sleep(1.0)
 
 if (confirm.upper() == 'Y') or (confirm.upper() == 'YES'):
-    time.sleep(1.0)
     run_confs = True
     print('---')
     time.sleep(1.0)
 else:
-    time.sleep(1.0)
     run_confs = False
     print('Commencing immediate startup...')
     time.sleep(0.5)
@@ -32,15 +36,14 @@ else:
 if run_confs:
     # -- Startup Confirmation --
     startup_confirm = input('[Confirmation] Would you like to start the bot? (y/n)   |  Answer: ')
+    time.sleep(1.0)
 
     if (startup_confirm.upper() == 'Y') or (startup_confirm.upper() == 'YES'):
-        time.sleep(1.0)
         print('Request for startup confirmed.')
         time.sleep(1.0)
         print('---')
         time.sleep(1.0)
     else:
-        time.sleep(1.0)
         print('Request for startup denied. Exiting bot\'s startup process...')
         time.sleep(1.0)
         print('[ --- STARTUP CONFIRMATIONS: END --- ]')
@@ -59,23 +62,60 @@ if run_confs:
 
     # -- Prefix Confirmation --
     prefix_confirm = input(f'[Confirmation] Would you like to keep the command prefix set to \'{prefix}\'? (y/n)   |  Answer: ')
+    time.sleep(1.0)
 
-    if (prefix_confirm.upper() == 'Y') or (prefix_confirm.upper() == 'YES') and run_confs:
-        time.sleep(1.0)
+    if (prefix_confirm.upper() == 'Y') or (prefix_confirm.upper() == 'YES'):
         print(f'Request for maintaining prefix confirmed.')
         time.sleep(0.5)
         print(f'Bot Prefix: \'{prefix}\'')
-    else:
+        prefix_perm = False
         time.sleep(1.0)
-        prefix = input('What would you like the command prefix to be set to? ')
+        print('---')
+    else:
+        prefix = input('What would you like the command prefix to be set to?   |  Prefix: ')
+        time.sleep(1.0)
+        prefix_perm_confirm = input('Would you like the specified prefix to be a permanent change? (y/n)   |  Answer: ')
+        if (prefix_perm_confirm.upper() == 'Y') or (prefix_perm_confirm.upper() == 'YES'):
+            prefix_perm = True
+        else:
+            prefix_perm = False
         time.sleep(0.5)
         print(f'Bot Prefix: \'{prefix}\'')
+        time.sleep(1.0)
+        print('---')
+
+    # -- CMC Data Confirmation --
+    time.sleep(1.0)
+    print('** Note: The following decision will be saved for future reference. **')
+    time.sleep(1.0)
+    data_confirm = input(f'[Confirmation] Would you like to load the CoinMarketCap data on startup? (y/n)   |  Answer: ')
+    time.sleep(1.0)
+
+    if (data_confirm.upper() == 'Y') or (data_confirm.upper() == 'YES'):
+        print('Request for data load on startup confirmed.')
+        data_perm = True
+        time.sleep(1.0)
+    else:
+        print('The CoinMarketData will not be loaded on startup. Please use the \'load-cmcdata\' command to load the data manually.')
+        data_perm = False
+        time.sleep(1.0)
+
+    
+    # -- Dump Data --
+    with open('src/hidden/secrets.json', 'w') as outfile:
+        if prefix_perm:
+            serverinfo['prefix'] = prefix
+            serverinfo['datastartup'] = data_perm
+        else:
+            serverinfo['datastartup'] = data_perm
+
+        json.dump(secrets, outfile, indent = 4)
+
 
     time.sleep(1.0)
     print('[ --- STARTUP CONFIRMATIONS: END --- ]')
     time.sleep(1.0)
     
-
 
 # --
 
@@ -117,13 +157,6 @@ async def unload(ctx, extension):
 async def reload(ctx, extension):
     client.unload_extension(f'cogs.{extension}')
     client.load_extension(f'cogs.{extension}')
-
-
-
-# -- Load Discord API Key --
-with open('src/hidden/secrets.json') as f: 
-    secrets = json.load(f)
-    keys = secrets['keys']
 
 
 
